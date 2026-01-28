00a6e45b6ff904e18e79c5f29d92db463bfd7b67
Llyjus <Llyjus@protonmail.com>
2026-01-28 14:35:24 +0000

    chore：更正文件名

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,11 +3,11 @@ from .test_intergration.test_gacha_in import test_gacha_process
 from .test_unit.test_gacha import test_gacha
 from .test_unit.test_interface import test_draw_card_success
 from .test_unit.test_db_init import test_db_init
-from .test_unit.test_cleaner import *
+from .test_unit.test_cleaner import test_cleaner
 
 # 6
 __all__ = ['test_users_table', 'test_cards_table'
            'test_gacha',
            'test_gacha_process',
-           'test_draw_card_success',
+           'test_draw_card_success', 'test_cleaner'
            'test_db_init']
